c299565be47cf5be0bc0e934c8abcc26022a4766
Martin Guillon <martin.guillon@akylas.fr>
2022-06-17 14:31:27 +0200

    fix: trying to fix debug build

--- scripts/build.android.sh
+++ scripts/build.android.sh
@@ -3,7 +3,6 @@
 source $(dirname $0)/env.sh
 
 ARCH_ARR=(arm arm64 x86 x64)
-BUILD_TYPE="release"
 while getopts 'l:b:' opt; do
   case ${opt} in
     l)
@@ -18,16 +17,24 @@ shift $(expr ${OPTIND} - 1)
 ## prepare configuration
 
 
+declare -A OUTPUT_FOLDERNAMES
+OUTPUT_FOLDERNAMES[arm]=armeabi-v7a
+OUTPUT_FOLDERNAMES[arm64]=arm64-v8a
+OUTPUT_FOLDERNAMES[x86]=x86
+OUTPUT_FOLDERNAMES[x64]=x86_64
+
+declare -A NDK_BUILD_TOOLS_ARR
+
 if [ "$(uname)" == "Darwin" ]; then
-        NDK_BUILD_TOOLS_ARR=([arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/arm-linux-androideabi/bin \
-                     [arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/aarch64-linux-android/bin \
-                     [x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/i686-linux-android/bin \
-                     [x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/x86_64-linux-android/bin)
+        NDK_BUILD_TOOLS_ARR[arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/arm-linux-androideabi/bin
+        NDK_BUILD_TOOLS_ARR[arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/aarch64-linux-android/bin
+        NDK_BUILD_TOOLS_ARR[x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/i686-linux-android/bin 
+        NDK_BUILD_TOOLS_ARR [x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/x86_64-linux-android/bin
 else
-        NDK_BUILD_TOOLS_ARR=([arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin \
-                     [arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin \
-                     [x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/i686-linux-android/bin \
-                     [x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/x86_64-linux-android/bin)
+        NDK_BUILD_TOOLS_ARR[arm]=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin
+        NDK_BUILD_TOOLS_ARR[arm64]=$ANDROID_NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin
+        NDK_BUILD_TOOLS_ARR[x86]=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/i686-linux-android/bin
+        NDK_BUILD_TOOLS_ARR[x64]=$ANDROID_NDK/toolchains/x86_64-4.9/prebuilt/linux-x86_64/x86_64-linux-android/bin
 fi
 
 # The order of CPU architectures in this array must be the same
@@ -35,27 +42,31 @@ fi
 
 BUILD_DIR_PREFIX="outgn"
 
+BUILD_TYPE="release"
+if [[ $1 == "debug" ]] ;then
+        BUILD_TYPE="debug"
+fi
 
 cd ${V8_DIR}
 # generate project in release mode
 for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
+        COMMON_ARGS="use_custom_libcxx=true use_goma=false is_clang=true enable_resource_allowlist_generation=false is_component_build=true v8_use_external_startup_data=false is_official_build=true target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false cc_wrapper=\"ccache\" android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_MAJOR_VERSION android_sdk_platform_version=$ANDROID_SDK_PLATFORM_VERSION android_sdk_build_tools_version=\"$ANDROID_SDK_BUILD_TOOLS_VERSION\"  android_sdk_version=$ANDROID_SDK_PLATFORM_VERSION android64_ndk_api_level=$NDK_64_API_LEVEL"
         if [[ $BUILD_TYPE == "debug" ]] ;then
-                ARGS="use_custom_libcxx=false is_component_build=true v8_use_external_startup_data=true is_debug=true symbol_level=2 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
-                gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_MAJOR_VERSION android_sdk_platform_version=$ANDROID_SDK_PLATFORM_VERSION android_sdk_build_tools_version=\"$ANDROID_SDK_BUILD_TOOLS_VERSION\"  android_sdk_version=$ANDROID_SDK_PLATFORM_VERSION android64_ndk_api_level=$NDK_64_API_LEVEL"
+                ARGS="is_official_build=false is_debug=true symbol_level=2"
         else
-                ARGS="use_custom_libcxx=false is_component_build=true v8_use_external_startup_data=true is_official_build=true use_thin_lto=false is_debug=false symbol_level=0 target_cpu=\"$CURRENT_ARCH\" v8_target_cpu=\"$CURRENT_ARCH\" v8_enable_i18n_support=false target_os=\"android\" v8_android_log_stdout=false"
-                gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$ARGS android32_ndk_api_level=$NDK_API_LEVEL android_ndk_major_version=$NDK_MAJOR_VERSION android_sdk_platform_version=$ANDROID_SDK_PLATFORM_VERSION android_sdk_build_tools_version=\"$ANDROID_SDK_BUILD_TOOLS_VERSION\"  android_sdk_version=$ANDROID_SDK_PLATFORM_VERSION android64_ndk_api_level=$NDK_64_API_LEVEL"
-
+                ARGS="is_official_build=true use_thin_lto=false is_debug=false symbol_level=0 "
         fi
+        gn gen $BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE --args="$COMMON_ARGS $ARGS"
 done
 
 # compile project
 for CURRENT_ARCH in ${ARCH_ARR[@]}
 do
+        echo "building $CURRENT_ARCH for $BUILD_TYPE"
 
         # make fat build
-        V8_FOLDERS=(v8_compiler v8_base_without_compiler v8_libplatform v8_libbase v8_libsampler v8_snapshot v8_initializers v8_init torque_generated_initializers)
+        V8_FOLDERS=(v8_compiler v8_base_without_compiler v8_libplatform v8_libbase v8_libsampler v8_snapshot v8_initializers v8_init torque_generated_initializers torque_generated_definitions)
         export CCACHE_CPP2=yes
 	export CCACHE_SLOPPINESS=time_macros
 	export PATH=$V8_DIR/third_party/llvm-build/Release+Asserts/bin:$PATH
@@ -67,61 +78,63 @@ do
 
         OUTFOLDER=${BUILD_DIR_PREFIX}/${CURRENT_ARCH}-${BUILD_TYPE}
         CURRENT_BUILD_TOOL=${NDK_BUILD_TOOLS_ARR[$CURRENT_ARCH]}
+        OUTPUT_FOLDERNAME=${OUTPUT_FOLDERNAMES[$CURRENT_ARCH]}
         V8_FOLDERS_LEN=${#V8_FOLDERS[@]}
         LAST_PARAM=""
-        CURRENT_DIST_DIR="$DIST_DIR/$CURRENT_ARCH-$BUILD_TYPE"
+        CURRENT_DIST_DIR="$DIST_DIR/$BUILD_TYPE/$OUTPUT_FOLDERNAME"
+        CURRENT_DIST_DIR_INCLUDES="$DIST_DIR/$BUILD_TYPE/"
         echo "CURRENT_ARCH $CURRENT_ARCH"
+        echo "OUTPUT_FOLDERNAME $OUTPUT_FOLDERNAME"
         echo "CURRENT_BUILD_TOOL $CURRENT_BUILD_TOOL"
+        echo "CURRENT_DIST_DIR $CURRENT_DIST_DIR"
+        echo "CURRENT_DIST_DIR_INCLUDES $CURRENT_DIST_DIR_INCLUDES"
 
         mkdir -p $CURRENT_DIST_DIR
 
 
-        if [[ $BUILD_TYPE == "debug" ]] ;then 
-                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/**/*.o"
-        else
-                for CURRENT_V8_FOLDER in ${V8_FOLDERS[@]}
-                do
-                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/${CURRENT_V8_FOLDER}/*.o"
-                done
+       
+        for CURRENT_V8_FOLDER in ${V8_FOLDERS[@]}
+        do
+        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/${CURRENT_V8_FOLDER}/*.o"
+        done
 
-                $CURRENT_BUILD_TOOL/ar r $CURRENT_DIST_DIR/libinspector_protocol.a $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o
-                $CURRENT_BUILD_TOOL/ranlib $CURRENT_DIST_DIR/libinspector_protocol.a
+        $CURRENT_BUILD_TOOL/ar r $CURRENT_DIST_DIR/libinspector_protocol.a $OUTFOLDER/obj/third_party/inspector_protocol/crdtp/*.o $OUTFOLDER/obj/third_party/inspector_protocol/crdtp_platform/*.o
+        $CURRENT_BUILD_TOOL/ranlib $CURRENT_DIST_DIR/libinspector_protocol.a
 
-                LAST_PARAM="${LAST_PARAM} $OUTFOLDER/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
+        LAST_PARAM="${LAST_PARAM} $OUTFOLDER/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_adler32_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/google/compression_utils_portable/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_inflate_chunk_simd/*.o"
 
-                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/android_ndk/cpu_features/*.o"
-                # LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/cppgc_base/*.o ${OUTFOLDER}/obj/v8_cppgc_shared/*.o"
-                
-                if [[ $CURRENT_ARCH = "arm" || $CURRENT_ARCH = "arm64" ]]; then
-                        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_arm_crc32/*.o"
-                fi
-
-                if [[ $CURRENT_ARCH = "x86" || $CURRENT_ARCH = "x64" ]]; then
-                        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_x86_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_crc32_simd/*.o"
-                fi
-
-                THIRD_PARTY_OUT=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/obj/buildtools/third_party
-                LAST_PARAM="${LAST_PARAM} $THIRD_PARTY_OUT/libc++/libc++/*.o $THIRD_PARTY_OUT/libc++abi/libc++abi/*.o"
+        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/android_ndk/cpu_features/*.o"
+        LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/cppgc_base/*.o ${OUTFOLDER}/obj/v8_cppgc_shared/*.o"
+        
+        if [[ $CURRENT_ARCH = "arm" || $CURRENT_ARCH = "arm64" ]]; then
+                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_arm_crc32/*.o"
         fi
+
+        if [[ $CURRENT_ARCH = "x86" || $CURRENT_ARCH = "x64" ]]; then
+                LAST_PARAM="${LAST_PARAM} ${OUTFOLDER}/obj/third_party/zlib/zlib_x86_simd/*.o ${OUTFOLDER}/obj/third_party/zlib/zlib_crc32_simd/*.o"
+        fi
+
+        THIRD_PARTY_OUT=$BUILD_DIR_PREFIX/$CURRENT_ARCH-$BUILD_TYPE/obj/buildtools/third_party
+        LAST_PARAM="${LAST_PARAM} $THIRD_PARTY_OUT/libc++/libc++/*.o $THIRD_PARTY_OUT/libc++abi/libc++abi/*.o"
         
         
         $CURRENT_BUILD_TOOL/ar r $CURRENT_DIST_DIR/libv8.a ${LAST_PARAM}
         
         # include files
-        rsync -r --exclude '.git' --exclude '.cache' --exclude 'DEPS' --exclude 'DIR_METADATA' "$V8_DIR/include/" "$CURRENT_DIST_DIR/include/"
-        rsync -r --exclude '.git' --exclude '.git' "${OUTFOLDER}/gen/include/" "$CURRENT_DIST_DIR/include/"
-        rsync -r --exclude '.git' --exclude '.git' "$V8_DIR/third_party/android_ndk/sources/cxx-stl/llvm-libc++/include/" "$CURRENT_DIST_DIR/include/libc++/"
+        rsync -r --exclude '.git' --exclude '.cache' --exclude 'DEPS' --exclude 'DIR_METADATA' "$V8_DIR/include/" "$CURRENT_DIST_DIR_INCLUDES/include/"
+        rsync -r --exclude '.git' --exclude '.git' "${OUTFOLDER}/gen/include/" "$CURRENT_DIST_DIR_INCLUDES/include/"
+        rsync -r --exclude '.git' --exclude '.git' "$V8_DIR/third_party/android_ndk/sources/cxx-stl/llvm-libc++/include/" "$CURRENT_DIST_DIR_INCLUDES/include/libc++/"
         
         # generated files
-        rsync -r --exclude '.git' --exclude '*.cache' --exclude 'snapshot.cc' --exclude 'embedded.S' "${OUTFOLDER}/gen/" "$CURRENT_DIST_DIR/generated/"
+        rsync -r --exclude '.git' --exclude '*.cache' --exclude 'snapshot.cc' --exclude 'embedded.S' "${OUTFOLDER}/gen/" "$CURRENT_DIST_DIR_INCLUDES/generated/"
         
-        mkdir -p "$CURRENT_DIST_DIR/v8_inspector"
+        mkdir -p "$CURRENT_DIST_DIR_INCLUDES/v8_inspector"
         INSPECTOR_LIST=($(grep "\"+.*\""  $V8_DIR/src/inspector/DEPS | sed 's/[+,"]//g'))
         # remove last one ../../third_party/inspector_protocol/crdtp
         unset INSPECTOR_LIST[-1]
 	INSPECTOR_LIST+=("base/trace_event")
 	INSPECTOR_LIST+=("src/base")
 	INSPECTOR_LIST+=("src/common")
-        rsync -auR --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "${INSPECTOR_LIST[@]}" "$CURRENT_DIST_DIR/v8_inspector/"
-        rsync -ur --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "$CURRENT_DIST_DIR/generated/src/" "$CURRENT_DIST_DIR/v8_inspector/src/"
+        rsync -auR --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "${INSPECTOR_LIST[@]}" "$CURRENT_DIST_DIR_INCLUDES/v8_inspector/"
+        rsync -ur --exclude '.git' --exclude '*.cache' --exclude 'DEPS' --exclude 'DIR_METADATA'  --exclude 'OWNERS' --exclude 'BUILD.gn' "$CURRENT_DIST_DIR_INCLUDES/generated/src/" "$CURRENT_DIST_DIR_INCLUDES/v8_inspector/src/"
 done
